282bd447f622d71e0ae0bb5d254d661c971c31e4
mkumar-svg <mkumar800534@gmail.com>
2024-08-27 19:07:47 +0530

    Micro Service Eureka Server Project 03 - Intra communication using Discovery Client Component

--- BootMSProj03-EurekaServer/src/main/java/com/nit/BootMsProj03EurekaServerApplication.java
+++ BootMSProj03-EurekaServer/src/main/java/com/nit/BootMsProj03EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class BootMsProj03EurekaServerApplication {
-
+	// using Discovery Client component
 	public static void main(String[] args) {
 		SpringApplication.run(BootMsProj03EurekaServerApplication.class, args);
 	}
