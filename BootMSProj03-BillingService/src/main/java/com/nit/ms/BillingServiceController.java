99682e9759a3d3ac6080af1191f965c7f611298c
mkumar-svg <mkumar800534@gmail.com>
2024-08-27 19:03:39 +0530

    Billing Service Micro Service Project 03 - Intra communication using LoadBalance Client Component

---
+++ BootMSProj03-BillingService/src/main/java/com/nit/ms/BillingServiceController.java
@@ -0,0 +1,21 @@
+package com.nit.ms;
+
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/billing-api")
+public class BillingServiceController {
+	
+	@GetMapping("/payment")
+	public ResponseEntity<String> doPayment() {
+		int amount = new Random().nextInt(1000000);
+		return new ResponseEntity<String>("Bill amount is:: "+ amount +" Payment can be using Cards, upi payment", HttpStatus.OK);
+	}
+
+}
