2d91bdf4206727e3252cd38961fecdfb411856bc
mkumar-svg <mkumar800534@gmail.com>
2024-08-27 19:54:27 +0530

    Micro Service Billing Service Project 05 - Intra communication using Feign Client Component

---
+++ BootMSProj05-BillingService/src/main/java/com/nit/ms/BillingServiceController.java
@@ -0,0 +1,35 @@
+package com.nit.ms;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/billing-api")
+public class BillingServiceController {
+	
+	@Value("${server.port}")
+	private int port;
+	
+	@Value("${eureka.instance.instance-id}")
+	private String instance_id;
+	
+	@GetMapping("/payment")
+	public ResponseEntity<String> doPayment() {
+		int amount = new Random().nextInt(1000000);
+		return new ResponseEntity<String>("Bill amount is:: "+ amount +" Payment can be using Cards, upi payment" + instance_id + "....." + port, HttpStatus.OK);
+	}
+	
+	@GetMapping("/payment/{cardNo}")
+	public ResponseEntity<String> doPayment1(@PathVariable("cardNo") long cardNo) {
+		int amount = new Random().nextInt(1000000);
+		return new ResponseEntity<String>("Bill amount is:: "+ amount +" Payment can be using Cards no:"+cardNo+ " with " + instance_id + "....." + port, HttpStatus.OK);
+	}
+
+}
